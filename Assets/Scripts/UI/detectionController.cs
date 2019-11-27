e4ed69c376954855b2eabf68d6c422d68d87191a
The Mountaineer <angporo@gmail.com>
2019-11-27 17:23:52 +0100

    QOL Changes to detectionController
    Quality of Life for better readable code after i couldnt solve a problem

--- Assets/Scripts/UI/detectionController.cs
+++ Assets/Scripts/UI/detectionController.cs
@@ -31,24 +31,37 @@ public class detectionController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        UpdateInSight();
+        //UpdateTimetoForgetAggro();
         UpdateDetection();
         canvas.gameObject.transform.LookAt(player.transform); // UI is always visible for player if he can see unit
     }
 
     private void UpdateDetection() {                          // Lerps Colors using the reaction time => how close you are to being detected
-        fillArea.fillAmount = enemySight.reactionTime / enemySight.MaxReactionTime;
+        fillArea.fillAmount = enemySight.timeToReact / enemySight.MaxReactionTime;
         if (fillArea.fillAmount >= 0f) {
             if (fillArea.fillAmount == 1) {
                 fillArea.color = alertColor;
             }
             else {
                 canvas.gameObject.SetActive(true);
-                fillArea.color = Color.Lerp(startColor, endColor, enemySight.reactionTime / enemySight.MaxReactionTime);
+                fillArea.color = Color.Lerp(startColor, endColor, enemySight.timeToReact / enemySight.MaxReactionTime);
                 //Debug.Log("Reaction Time: "+ enemySight.reactionTime);
             }
         }
         else {
             canvas.gameObject.SetActive(false);
+        }
+    }
+
+    private void UpdateInSight() {                                                                          // Hält den Gegner davon ab fertig zu suchen, bevor er den Spieler sieht
+        if (enemySight.playerInSight)
+        {
+            enemySight.timeToReact += Time.deltaTime;
+        }
+        else/* if (enemySight.timeToForgetAggro <= 0)*/                                                        // Hat der Gegner seine Aggro auf den Spieler schon vergessen?
+        {
+            enemySight.timeToReact -= Time.deltaTime;                                                      // Gegner vergisst Spieler komplett
         }
     }
 }
