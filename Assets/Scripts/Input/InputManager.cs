db0d73794aa5d95bfe2471fde59f730076fc3061
The Mountaineer <angporo@gmail.com>
2019-11-29 00:45:52 +0100

    Creation of a cutsom Input system & more UI fuckery

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public static InputManager instance;
+
+    public Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+
+    private void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    public void Start()
+    {
+        LoadKeyBindings();
+    }
+
+    public bool GetKeyDown(string Action) {
+        return Input.GetKeyDown(keys[Action]);
+    }
+
+    public bool GetKeyUp(string Action) {
+        return Input.GetKeyUp(keys[Action]);
+    }
+
+    public bool GetKey(string Action) {
+        return Input.GetKey(keys[Action]);
+    }
+
+    public void LoadKeyBindings() { // Gets keybinds from Playerprefs and has defaults 
+        //Movement
+        keys.Add("Forward", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Forward", "W")));
+        keys.Add("Back", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Back", "S")));
+        keys.Add("Left", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "A")));
+        keys.Add("Right", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right", "D")));
+        keys.Add("Jump", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Jump", "Space")));
+        keys.Add("Sneak", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Sneak", "C")));
+
+        //Weaponhandling
+        keys.Add("Shoot", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Shoot", "Mouse0")));
+        keys.Add("Reload", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Reload", "R")));
+    }
+}
---
+++ Assets/Scripts/Input/InputMenuHandler.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/*
+ * Dieser InputMenuHandler verwaltet alle Aktionen im UI Menü, wo man die Inputkeys verändern kann
+ */
+public class InputMenuHandler : MonoBehaviour
+{
+    private Dictionary<string, KeyCode> keys;
+    public Button[] keyButton;
+    private Text[] keyText;
+
+    private GameObject currentKey;
+
+    public Color32 buttonNormal;
+    public Color32 buttonEditing;
+
+    private void Awake()
+    {
+        keys = GameObject.FindObjectOfType<InputManager>().keys;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; i < keyButton.Length; i++) {
+            keyText[i] = keyButton[i].transform.GetComponentInChildren<Text>();
+        }
+
+        // set Button text acording to set Buttons
+        int j = 0;
+        foreach (var key in keys) {
+            keyText[j].text = key.Key.ToString();
+            j++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnGUI()
+    {
+        if (currentKey != null) {
+            Event e = Event.current;
+            if (e.isKey) {
+                keys[currentKey.name] = e.keyCode;
+                currentKey.transform.GetComponentInChildren<Text>().text = e.keyCode.ToString();
+                currentKey.GetComponent<Image>().color = buttonNormal;
+                currentKey = null;
+            }
+        }
+    }
+
+    public void ChangeKey(GameObject clicked) { //Trigger for OnGUI to run
+        if (currentKey != null)
+        {
+            currentKey.GetComponent<Image>().color = buttonNormal;
+        }
+        currentKey = clicked;
+        currentKey.GetComponent<Image>().color = buttonEditing;
+    }
+
+    public void SaveKeys() {        // Save keys to PlayerPrefs
+        foreach (var key in keys) {
+            PlayerPrefs.SetString(key.Key,key.Value.ToString());
+        }
+        PlayerPrefs.Save();
+    }
+}
