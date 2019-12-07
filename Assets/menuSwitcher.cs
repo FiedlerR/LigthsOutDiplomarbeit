7aa9a3ea9749056901c89a1c91f4ba64fa5fa373
The Mountaineer <angporo@gmail.com>
2019-12-07 22:48:00 +0100

    Implemented Menu UI
    New Menu UI :+1:

---
+++ Assets/menuSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/*
+ * MenuSwitcher
+ * Der MenuSwitcher hat die Aufgabe zwischen verschiedenen Pausenmenu-Seiten zu wechseln
+ */
+public class menuSwitcher : MonoBehaviour
+{
+    public GameObject enableThis;
+    public GameObject disableThis;
+
+    public void SwitchMenu() {
+        disableThis.SetActive(false);
+        enableThis.SetActive(true);
+    }
+
+    public void closeMenu() {
+        disableThis.SetActive(false);
+    }
+}
