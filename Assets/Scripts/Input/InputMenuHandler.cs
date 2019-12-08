f463f96b542cd7913948b5884d062def36624e69
The Mountaineer <angporo@gmail.com>
2019-12-08 16:53:45 +0100

    FIxxed some more input UI stuff

--- Assets/Scripts/Input/InputMenuHandler.cs
+++ Assets/Scripts/Input/InputMenuHandler.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class InputMenuHandler : MonoBehaviour
 {
     private Dictionary<string, KeyCode> keys;
+    private Dictionary<string, KeyCode> backupKeys;
     public Button[] keyButton;
     private Text[] keyText;
 
@@ -19,12 +20,13 @@ public class InputMenuHandler : MonoBehaviour
     private void Awake()
     {
         keys = GameObject.FindObjectOfType<InputManager>().keys;
+        backupKeys = keys;
     }
     // Start is called before the first frame update
     void Start()
     {
         for (var i = 0; i < keyButton.Length; i++) {
-            keyText[i] = keyButton[i].transform.GetComponentInChildren<Text>();
+            keyText[i] = keyButton[i].GetComponentInChildren<Text>();
         }
 
         // set Button text acording to set Buttons
@@ -35,19 +37,52 @@ public class InputMenuHandler : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
     private void OnGUI()
     {
         if (currentKey != null) {
             Event e = Event.current;
-            if (e.isKey) {
+            if (e.isKey)
+            {
                 keys[currentKey.name] = e.keyCode;
                 currentKey.transform.GetComponentInChildren<Text>().text = e.keyCode.ToString();
-                currentKey.GetComponent<Image>().color = buttonNormal;
+                currentKey.GetComponentInChildren<Image>().color = buttonNormal;
+                currentKey = null;
+            }
+            else if (e.shift) {
+                keys[currentKey.name] = e.keyCode;
+                currentKey.transform.GetComponentInChildren<Text>().text = "Shift";
+                currentKey.GetComponentInChildren<Image>().color = buttonNormal;
+                currentKey = null;
+            }
+            else if (e.isMouse)
+            {
+                string keyString = "MouseCode";
+                switch (e.keyCode) {
+                    case KeyCode.Mouse0:
+                        keyString = "Mouse0";
+                        break;
+                    case KeyCode.Mouse1:
+                        keyString = "Mouse1";
+                        break;
+                    case KeyCode.Mouse2:
+                        keyString = "Mouse2";
+                        break;
+                    case KeyCode.Mouse3:
+                        keyString = "Mouse3";
+                        break;
+                    case KeyCode.Mouse4:
+                        keyString = "Mouse4";
+                        break;
+                    case KeyCode.Mouse5:
+                        keyString = "Mouse5";
+                        break;
+                    case KeyCode.Mouse6:
+                        keyString = "Mouse6";
+                        break;
+                }
+                keys[currentKey.name] = e.keyCode;
+                currentKey.transform.GetComponentInChildren<Text>().text = keyString;
+                currentKey.GetComponentInChildren<Image>().color = buttonNormal;
                 currentKey = null;
             }
         }
@@ -56,10 +91,10 @@ public class InputMenuHandler : MonoBehaviour
     public void ChangeKey(GameObject clicked) { //Trigger for OnGUI to run
         if (currentKey != null)
         {
-            currentKey.GetComponent<Image>().color = buttonNormal;
+            currentKey.GetComponentInChildren<Image>().color = buttonNormal;
         }
         currentKey = clicked;
-        currentKey.GetComponent<Image>().color = buttonEditing;
+        currentKey.GetComponentInChildren<Image>().color = buttonEditing;
     }
 
     public void SaveKeys() {        // Save keys to PlayerPrefs
@@ -67,5 +102,16 @@ public class InputMenuHandler : MonoBehaviour
             PlayerPrefs.SetString(key.Key,key.Value.ToString());
         }
         PlayerPrefs.Save();
+    }
+
+    public void BackButNoSave()
+    {
+        Debug.Log("NoSave was triggered");
+        keys = backupKeys;
+        int i = 0;
+        foreach (var key in keys) {
+            keyText[i].text = key.Key.ToString();
+            i++;
+        }
     }
 }
